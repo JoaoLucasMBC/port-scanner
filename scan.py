0116e91491de99a1da3c82d47185bebfca930ece
Joao Lucas <joaolucasmbcadorniga@gmail.com>
2024-08-25 22:13:32 -0300

    FEAT: first version ready in the terminal

---
+++ scan.py
@@ -0,0 +1,92 @@
+import pyfiglet
+import sys
+import socket
+import multiprocessing
+import json
+
+def main():
+    ascii_banner = pyfiglet.figlet_format("PORT SCANNER")
+    print(ascii_banner)
+
+    if len(sys.argv) > 4 or len(sys.argv) < 2:
+        print("Invalid arguments")
+        print("Usage: python3 scan.py <ip/host> <start_port> <end_port>")
+        sys.exit()
+    
+    target = sys.argv[1]
+
+    if not target.isnumeric():
+        try:
+            target = socket.gethostbyname(target)    
+        except socket.gaierror:
+            print("\n Hostname couldn't be resolved. Check the target or try again later.")
+            sys.exit()
+    
+    print("Scanning Target: " + target)
+
+    print("-=" * 10 + '\n')
+    
+    if len(sys.argv) == 2:
+        start_port = None
+        end_port = None
+    else:
+        start_port = int(sys.argv[2])
+        end_port = int(sys.argv[3])
+
+    try:
+        # Create a multiprocessing manager to share data between processes
+        with multiprocessing.Manager() as manager:
+            closed_ports = manager.list()  # Shared list to store closed ports
+            
+            num_processos = multiprocessing.cpu_count()
+            pool = multiprocessing.Pool(processes=num_processos)
+
+            # Check if the user provided a port range
+            if start_port is None:
+                for port in MAIN_PORTS:
+                    pool.apply_async(scan_port, args=(target, int(port), closed_ports))
+            else:
+                for port in range(start_port, end_port+1):
+                    pool.apply_async(scan_port, args=(target, port, closed_ports))
+
+            pool.close()
+            pool.join()
+
+            # Print all closed ports at once
+            if closed_ports:
+                print("\nClosed Ports:")
+                print(", ".join(str(port) for port in closed_ports))
+            else:
+                print("No closed ports found.")
+
+    except KeyboardInterrupt:
+        print("\n Exiting Program")
+        sys.exit()
+
+
+def read_main_ports():
+    with open("main_ports.json", "r") as f:
+        return json.load(f)
+
+
+def scan_port(target, port, closed_ports):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(1)
+        result = s.connect_ex((target, port))
+        if result == 0:
+            service = MAIN_PORTS.get(str(port), "Unknown Service")
+            print(f"Port {port} is open. Service: {service}")
+        else:
+            closed_ports.append(port)  # Append closed port to the shared list
+        s.close()
+    except socket.timeout:
+        print(f"Timeout occurred for port: {port}")
+    except socket.error:
+        print(f"\n Server not responding for port: {port}")
+    finally:
+        s.close()
+
+if __name__ == "__main__":
+    MAIN_PORTS = read_main_ports()
+    main()
---
+++ well-known-ports.py
@@ -0,0 +1,46 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+
+def scrape_port_services():
+    print("Scraping port services from Wikipedia...")
+
+    url = "https://en.wikipedia.org/wiki/List_of_TCP_and_UDP_port_numbers"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    port_service_map = {}
+
+    tables = soup.find_all("table", {"class": "wikitable"})
+    print(len(tables))
+    for table in tables[1:3]:
+        rows = table.find_all("tr")
+        for row in rows[1:]:  # Pula o cabecalho
+            cols = row.find_all("td")
+            if len(cols) >= 2:
+                try:
+                    if "Yes" in cols[1].text:
+                        port_range = cols[0].text.strip()
+                        service_name = cols[-1].text.strip()
+
+                        cleaned_text = re.sub(r'\[.*?\]', '', service_name)
+                        
+                        print(port_range, cleaned_text)
+                        port_service_map[int(port_range)] = cleaned_text
+                except:
+                    continue
+
+    return port_service_map
+
+
+
+if __name__ == "__main__":
+    port_service_map = scrape_port_services()
+
+    for port, service in list(port_service_map.items())[:20]: 
+        print(f"Port {port}: {service}")
+    
+    # Salve eme um arquivo JSON
+    import json
+    with open("main_ports.json", "w") as f:
+        json.dump(port_service_map, f)
